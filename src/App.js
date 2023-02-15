0de265756c0a707894c24e9494ac6cbed8995465
Joshva02 <109680659+Joshva02@users.noreply.github.com>
2023-02-15 15:04:32 -0400

    Added Cart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ProductDetails from './pages/ProductDetails'
 import Sidebar from './components/Sidebar'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Cart from './pages/Cart';
 
 const App = () => {
   return <div className='overflow-hidden'>
@@ -15,6 +16,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/cart' element={<Cart />} />
         </Routes> 
         <Sidebar />
         <Footer />
